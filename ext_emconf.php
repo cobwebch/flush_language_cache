a80752c0ea8c593b0f1efe0f9748757168f4c2ea
Francois Suter <francois@typo3.org>
2014-12-23 14:24:29 +0100

    Verified compatibility with TYPO3 CMS 7.x

--- ext_emconf.php
+++ ext_emconf.php
@@ -22,16 +22,16 @@ $EM_CONF[$_EXTKEY] = array (
 	'author_email' => 'typo3@cobweb.ch',
 	'author_company' => '',
 	'version' => '1.0.0',
-	'constraints' => 
+	'constraints' =>
 	array (
-		'depends' => 
+		'depends' =>
 		array (
-			'typo3' => '6.2.0-6.2.99',
+			'typo3' => '6.2.0-7.99.99',
 		),
-		'conflicts' => 
+		'conflicts' =>
 		array (
 		),
-		'suggests' => 
+		'suggests' =>
 		array (
 		),
 	),
