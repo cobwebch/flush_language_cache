6cef3eb7f447ec4afd75f12d8e9a8ba4341ba73e
Roberto Presedo <roberto.presedo@ideative.ch>
2021-10-01 12:20:28 +0200

    v3.0.0 TYPO3 v11 compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,11 +15,11 @@ $EM_CONF[$_EXTKEY] = [
         'author' => 'Francois Suter',
         'author_email' => 'typo3@cobweb.ch',
         'author_company' => '',
-        'version' => '2.2.3',
+        'version' => '3.0.0',
         'constraints' => [
                 'depends' => [
-                        'typo3' => '9.5.0-10.4.99',
-                        'php' => '7.2.0-7.4.99'
+                        'typo3' => '9.5.0-11.4.99',
+                        'php' => '7.2.0-8.0.99'
                 ],
                 'conflicts' => [],
                 'suggests' => [],
