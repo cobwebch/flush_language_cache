354ba7254101f58961cd7dec7caf3b92f97a87d0
Francois Suter <>
2021-10-08 21:59:40 +0200

    [BUGFIX] Fix TYPO3 compatibility range, raise version to 3.0.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,10 +15,10 @@ $EM_CONF[$_EXTKEY] = [
         'author' => 'Francois Suter',
         'author_email' => 'typo3@cobweb.ch',
         'author_company' => '',
-        'version' => '3.0.0',
+        'version' => '3.0.1',
         'constraints' => [
                 'depends' => [
-                        'typo3' => '9.5.0-11.4.99',
+                        'typo3' => '9.5.0-11.5.99',
                         'php' => '7.2.0-8.0.99'
                 ],
                 'conflicts' => [],
