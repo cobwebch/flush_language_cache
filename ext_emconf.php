399dd28188d3f64ff1c2e510d6e94584016b3e8c
François Suter <fsuter@users.noreply.github.com>
2024-05-08 08:22:25 +0200

    Prepare for TYPO3 13 compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -18,8 +18,8 @@ $EM_CONF[$_EXTKEY] = [
         'version' => '4.0.1',
         'constraints' => [
                 'depends' => [
-                        'typo3' => '11.5.0-12.4.99',
-                        'php' => '7.4.0-8.2.99'
+                        'typo3' => '12.4.0-13.1.99',
+                        'php' => '8.1.0-8.3.99'
                 ],
                 'conflicts' => [],
                 'suggests' => [],
